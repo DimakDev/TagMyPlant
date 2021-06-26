6d156817fc6b9cd26d382c0a4f2490a64cbd16bd
DimakDev <private.dk@icloud.com>
2021-06-26 08:00:31 +0200

    Added CodeScanner view

--- TagMyPlant/ContentView.swift
+++ TagMyPlant/ContentView.swift
@@ -6,11 +6,28 @@
 //
 
 import SwiftUI
+import CarBode
+import AVFoundation
+
 
 struct ContentView: View {
+    
+    @State var isPresentingScanner = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            CBScanner(
+                supportBarcode: .constant([.qr, .code128]),
+                scanInterval: .constant(5.0),
+                mockBarCode: .constant(BarcodeData(value:"Mocking data", type: .qr))
+            ){
+                print("BarCodeType =",$0.type.rawValue, "Value =",$0.value)
+                self.isPresentingScanner = false
+            }
+            VStack {
+                Text("Scan the code")
+            }
+        }
     }
 }
 
