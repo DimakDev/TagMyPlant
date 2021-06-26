f09a884c9b2ad3c48c0b1fcc758b9c3d0956769f
DimakDev <private.dk@icloud.com>
2021-06-26 06:40:33 +0200

    Initial Commit

---
+++ TagMyPlant/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TagMyPlant
+//
+//  Created by Dmytro Kostiuk on 26.06.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TagMyPlant/TagMyPlantApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagMyPlantApp.swift
+//  TagMyPlant
+//
+//  Created by Dmytro Kostiuk on 26.06.21.
+//
+
+import SwiftUI
+
+@main
+struct TagMyPlantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
