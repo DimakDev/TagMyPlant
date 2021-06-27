3d565fd79c5facf8d6ddcbc0ea1f499743598395
DimakDev <private.dk@icloud.com>
2021-06-27 04:19:35 +0200

    Added BarcodeScannerController as ObservableObject to parse and hold barcode metadata

--- TagMyPlant/BarcodeScannerController.swift
+++ TagMyPlant/BarcodeScannerController.swift
@@ -1,5 +1,5 @@
 //
-//  CodeDataModel.swift
+//  BarcodeScannerController.swift
 //  TagMyPlant
 //
 //  Created by Dmytro Kostiuk on 26.06.21.
@@ -8,7 +8,54 @@
 import Foundation
 
 final class BarcodeScannerController: ObservableObject {
-    func storeBarcodeMetadata(barcodeType: String, barcodeValue: String) {
-        var barcode = BarcodeModel()
+    
+    @Published var barcodes: [Barcode] = []
+    
+    func storeBarcodeMetadata(barcodeType: String, barcodeContentString: String) {
+        let parsedBarcodeContent = parseBarcodeContent(barcodeContentString: barcodeContentString)
+        let barcode = Barcode(type: barcodeType, content: parsedBarcodeContent)
+        self.barcodes.append(barcode)
+    }
+    
+    func parseBarcodeContent(barcodeContentString: String) -> [BarcodeContent] {
+        let detector = try! NSDataDetector(types: NSTextCheckingAllTypes)
+        let range = NSRange(location: 0, length: barcodeContentString.utf16.count)
+        let matches = detector.matches(in: barcodeContentString, options: [], range: range)
+        
+        var barcodeContent: [BarcodeContent] = []
+        
+        for match in matches {
+            switch match.resultType {
+            case .link:
+                guard let url = match.url else { continue }
+                if url.absoluteString.hasPrefix("mailto:") {
+                    let email = url.absoluteString.replacingOccurrences(of: "mailto:", with: "")
+                    let content = BarcodeContent.email(email)
+                    barcodeContent.append(content)
+                } else if url.absoluteString.contains("@") {
+                    let email = url.absoluteString
+                    let content = BarcodeContent.email(email)
+                    barcodeContent.append(content)
+                } else {
+                    let content = BarcodeContent.url(url.absoluteString)
+                    barcodeContent.append(content)
+                }
+            case .date:
+                guard let date = match.date else { continue }
+                guard let timeZone = match.timeZone else { continue }
+                let duration = match.duration
+                let dateString = "Date: \(date), TimeZone: \(timeZone), Duration: \(duration)"
+                let content = BarcodeContent.date(dateString)
+                barcodeContent.append(content)
+            case .phoneNumber:
+                guard let phoneNumber = match.phoneNumber else { continue }
+                let content = BarcodeContent.phoneNumber(phoneNumber)
+                barcodeContent.append(content)
+            default:
+                print(match)
+            }
+        }
+        
+        return barcodeContent
     }
 }
